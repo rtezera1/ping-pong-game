780a46b2ec69f9548f3a449777e212492101c658
Robel <robeltadesse01@gmail.com>
2014-05-07 07:12:48 -0400

    initial commit

---
+++ Main.js
@@ -0,0 +1,25 @@
+var canvas; //Will be linked to the canvas in our index.html page
+var stage; //Is the equivalent of stage in AS3; we'll add chidren
+
+//Graphics
+//[Background]
+
+var bg; //The background Graphics
+
+//[Title View]
+
+var main; //The Main Background 
+var startB; //The Start button in the main menu
+var creditsB; //The credits button in the main menu
+
+//[Credits]
+
+var credits; //The Credits scrren
+//[Game View]
+
+var player; //The player paddle Graphics
+var ball; //The ball Graphics
+var cpu; //The CPU paddle
+var win; //The winning popup
+var lose; //The losing popup
+
